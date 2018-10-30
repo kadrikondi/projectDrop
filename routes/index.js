ca81f850e305f7cba47bcd2ebbf484c004a47f45
kadrikondi <abdulkadri42@gmail.com>
2018-10-30 16:37:32 +0100

    change project and users routes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const express= require('express');
 const router = express.Router();
 const path = require('path')
+
+const project =require('../models/project')
 const userController= require('../controllers/userController')
 const authController =require('../auth/authController');
 const {verifyToken}=require('../auth/verifyToken')
@@ -13,50 +15,50 @@ const projectController =require('../controllers/projectContorller')
 const multer = require('multer')
 
 //define multer storage and file name
-const storage = multer.diskStorage({
-    destination:(req, file, cb)=>{
-        cb(null, './uploadProjects/')
-    },
-    filename:(req,file, cb)=>{
-        cb(null, file.originalname +"-" + Date.now() + path.extname(file.originalname))
-    }
-})
+// const storage = multer.diskStorage({
+//     destination:(req, file, cb)=>{
+//         cb(null, './uploadProjects/')
+//     },
+//     filename:(req,file, cb)=>{
+//         cb(null, file.originalname +"-" + Date.now() + path.extname(file.originalname))
+//     }
+// })
 
-//configure multer filter
-const fileFilter = (req, file, cb)=>{
-    const filetypes = /docx|doc|msword|rtf|zip|rar|pdf|png|txt/;
-    //check extname
-    const extname = filetypes.test(path.extname(file.originalname).toLowerCase())
-    const mimetype =filetypes.test(file.mimetype)
-    if(mimetype && extname){
-        return cb(null,true)
-    }else{
+// //configure multer filter
+// const fileFilter = (req, file, cb)=>{
+//     const filetypes = /docx|doc|msword|rtf|zip|rar|pdf|png|txt/;
+//     //check extname
+//     const extname = filetypes.test(path.extname(file.originalname).toLowerCase())
+//     const mimetype =filetypes.test(file.mimetype)
+//     if(mimetype && extname){
+//         return cb(null,true)
+//     }else{
        
-    cb(new Error("upload only pdf ,doc or zip file"))
+//     cb(new Error("upload only pdf ,doc or zip file"))
     
 
-    }
-// application/vnd.openxmlformats-officedocument.wordprocessingml.document
-// "application/pdf" "application/zip" "application/x-rar-compressed"
+//     }
+// // application/vnd.openxmlformats-officedocument.wordprocessingml.document
+// // "application/pdf" "application/zip" "application/x-rar-compressed"
     
 
  
-}
+// }
 
-    const upload = multer({storage:storage, 
-                            limits:{fileSize:1024*1024*10},
-                            fileFilter:fileFilter
+//     const upload = multer({storage:storage, 
+//                             limits:{fileSize:1024*1024*10},
+//                             fileFilter:fileFilter
                 
-    }).single('projectdoc')
+//     }).single('projectdoc')
 
 
-
+    module.exports=upload
 //user route configuration and link
 const auth =passport.authenticate('jwt',{session:false})
 router.post('/register' ,userController.posttRegisterUser);
 router.post('/login',userController.postLogin);
 router.put('/update/:id',auth, userController.updateUserProfile)
-router.get('/users',auth,userController.getUsers)
+router.get('/users',userController.getUsers)
 router.route('/user')
 .get(verifyToken, userController.getUser)
 
@@ -67,14 +69,15 @@ router.delete('/delete/:id', userController.DeleteOne)
 
 //project controller
 
-//project route controller
-router.route('/uploadproject')
-             .post(auth,upload,projectController.createProject)
-router.route('/updateproject/:id')
+// //project route controller
+router.route('/project/post')
+             .post(projectController.createProject)
+//             
+router.route('/project/update/:id')
              .put(projectController.updateProject)
-router.route('/projects')
+router.route('/project/get')
              .get(projectController.getAllProjects)
-router.route('/project/:id')
+router.route('/project/get/:id')
             .get(projectController.getSingleProject)
 router.route('/project/like/:id')
             .post(auth,projectController.likeProject)
@@ -84,9 +87,40 @@ router.route('/project/comment/:id')
             .post(auth,projectController.createComment)
  router.route('/project/comment/:id/:comment_id')
             .delete(auth,projectController.deleteComment)
+router.delete('/project/delete/:id', auth, projectController.Deleteproject)
 
-router.delete('/deleteproject/:id', auth, projectController.Deleteproject)
 
 
+
+//Using cloudinary
+const storage = multer.diskStorage({
+    filename:function(req, file, cb){
+        cb(null, Date.now()+file.originalname)
+    }
+})
+const docFilter = function(req, file, cb){
+    if(!file.originalname.match(/\.(docx|doc|msword|rtf|zip|rar|pdf|txt)$/i)){
+        //return cb(new Error('Only image files are allowed'), false)
+        return cb('Only  zip or pdf or doc and docx files are allowed', false)
+    }
+    else{
+        cb(null,true)
+    }
+}
+var upload = multer({
+    storage:storage,
+    limits:{fileSize:1024*1024*10},
+    fileFilter:docFilter
+}).single('projectdoc')
+var config = require('../config/config')
+var cloudinary = require('cloudinary')
+console.log(config.cloud_name)
+cloudinary.config({
+    cloud_name: config.cloud_name,
+    api_key : config.api_key,
+    api_secret : config.api_secret
+})
+
+router.put('/project/upload/:id', upload,projectController.UploadProject) 
 
 module.exports=router;
