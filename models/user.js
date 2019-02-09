4f5810c8679f350c75697d09b90b781c3ff2155e
kadrikondi <abdulkadri42@gmail.com>
2019-02-09 16:04:27 +0100

    =add forget pass model

--- models/user.js
+++ models/user.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 
 const Users = mongoose.Schema({
     email: {type:String},
-    password:{type:String},
+    password:{type:String,require:true},
     name:{type:String},
     school:{type:String},
     phone:{type:String},
@@ -14,6 +14,8 @@ const Users = mongoose.Schema({
     bio:{type:String},
     avater:{type:String},
     join:  {type:Date, Default:Date.now() },
+    resetPasswordToken: String,
+   resetPasswordExpires: Date,
 
     
 
