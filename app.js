2e40e3bf7ad0ecf68ae7b456ab04b57f8a018d02
kadrikondi <abdulkadri42@gmail.com>
2019-03-29 11:32:03 +0100

    setup prduction

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const express= require('express')
+const express = require('express')
 const passport = require('passport')
-const mongoose= require('mongoose')
-const bodyPaser= require('body-parser')
+const mongoose = require('mongoose')
+const bodyPaser = require('body-parser')
 const cors = require('cors')
 const path = require('path')
 require('dotenv').config()
@@ -14,9 +14,11 @@ app.use(cors())
 
 //initialize public folder
 app.use('/uploadproject', express.static('uploadProjects'))
-app.use(bodyPaser.urlencoded({extended:true}))
+app.use(bodyPaser.urlencoded({
+    extended: true
+}))
 app.use(bodyPaser.json())
-app.use('/' , router)
+app.use('/', router)
 
 
 
@@ -25,35 +27,45 @@ app.use(passport.initialize());
 
 // Passport Config
 require('./config/passport')(passport);
+if (process.env.NODE_ENV === 'production') {
 
 
-app.use(express.static(path.resolve(__dirname, './client/build')))
-app.use(express.static(path.resolve(__dirname, 'build', 'index.html')))
-console.log()
-const port = process.env.PORT|| 8000;
-
-app.get('*', (req, res) => {
-    res.send(`<h1>ERORR 404, PAGE NOT FOUND</h1>`)
-})
+    app.use(express.static(path.resolve(__dirname, 'client/build')))
+    app.use(express.static(path.resolve(__dirname, 'build', 'index.html')))
+    console.log()
 
 
-app.listen(port,()=>{
-    
-mongoose.connect('mongodb://localhost:27017/projectdrop',{ useNewUrlParser: true })
-    .then(()=>{
-
-        console.log("mongodb connected")
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
+        // res.send(`<h1>ERORR 404, PAGE NOT FOUND</h1>`)
     })
-    .catch((err)=>{
-        console.log(err)
-    })||
-    mongoose.connect('mongodb://eventmanager:kadzee222@ds231740.mlab.com:31740/kondipressdb')
+}
+const port = process.env.PORT || 8000;
 
-    
+app.listen(port, () => {
+    if (process.env.NODE_ENV === 'production') {
+        mongoose.connect('mongodb://eventmanager:kadzee222@ds231740.mlab.com:31740/kondipressdb', {
+                useNewUrlParser: true
+            })
+            .then(() => {
+
+                console.log("mongodb connected online")
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    } else {
+        mongoose.connect('mongodb://localhost:27017/projectdrop', {
+                useNewUrlParser: true
+            })
+            .then(() => {
+
+                console.log("mongodb connected offline")
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+
+    }
     console.log(`our app is listening on port ${port}`)
 })
-
-
-
-
-
